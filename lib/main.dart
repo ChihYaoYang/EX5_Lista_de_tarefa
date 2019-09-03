5bbe406a740eccb6f4559e378668060f7d6e37f6
Marcos <marcos.camberto@aluno.sc.senac.br>
2019-09-03 00:55:29 -0300

    arruma erro

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,8 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   final _toDoController = TextEditingController();
   List _toDoList = [];
+  int _lastRemovedPos;
+  Map<String, dynamic> _lastRemoved;
 
   //@override sobreescrever método
   @override
@@ -37,6 +39,22 @@ class _HomeState extends State<Home> {
       _toDoList.add(newToDo);
       _saveData();
     });
+  }
+
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {
+      _toDoList.sort((a, b) {
+        if (a['ok'] && !b['ok']) {
+          return 1;
+        } else if (!a['ok'] && b['ok']) {
+          return -1;
+        } else {
+          return 0;
+        }
+      });
+    });
+    return null;
   }
 
   @override
@@ -70,15 +88,16 @@ class _HomeState extends State<Home> {
                     _addToDo();
                   },
                 ),
-                Expanded(
-                  child: RefreshIndicator(
-                      child: ListView.builder(
-                          padding: EdgeInsets.only(top: 10),
-                          itemCount: _toDoList.length,
-                          itemBuilder: buildItem),
-                      onRefresh: null),
-                ),
               ],
+            ),
+          ),
+          Expanded(
+            child: RefreshIndicator(
+              onRefresh: _refresh,
+              child: ListView.builder(
+                  padding: EdgeInsets.only(top: 10),
+                  itemCount: _toDoList.length,
+                  itemBuilder: buildItem),
             ),
           ),
         ],
@@ -101,20 +120,47 @@ class _HomeState extends State<Home> {
       ),
       direction: DismissDirection.startToEnd,
       child: CheckboxListTile(
-        title: Text("Text"),
-        value: null,
-        onChanged: (c) {},
+        title: Text(_toDoList[index]['title']),
+        value: _toDoList[index]['ok'],
+        onChanged: (c) {
+          setState(() {
+            _toDoList[index]['ok'] = c;
+            _saveData();
+          });
+        },
         secondary: CircleAvatar(
-          child: Icon(Icons.warning),
+          child: Icon(
+              _toDoList[index]['ok'] == true ? Icons.check : Icons.warning),
         ),
       ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+          _saveData();
+          final Snack = SnackBar(
+            content: Text("Tarefa ${_lastRemoved['title']} removida"),
+            action: SnackBarAction(
+                label: "Desfazer",
+                onPressed: () {
+                  setState(() {
+                    _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                    _saveData();
+                  });
+                }),
+            duration: Duration(seconds: 3),
+          );
+          Scaffold.of(context).showSnackBar(Snack);
+        });
+      },
     );
   }
 
 //retorna arquivo
   Future<File> _getFile() async {
     final directory = await getApplicationDocumentsDirectory();
-    return File("${directory}/data.json");
+    return File("${directory.path}/data.json");
   }
 
 //salvar
